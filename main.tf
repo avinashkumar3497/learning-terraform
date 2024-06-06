2bf2c7068534e7b3af34aa6ec6bc8a2eb3d84678
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-07 02:17:00 +0530

    aws credentials stored in cloud

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.key
-  secret_key = var.secret_key
+  //access_key = var.key
+  //secret_key = var.secret_key
 }
 
 resource "aws_instance" "demo_server" {
