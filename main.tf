f19ae77d8fa1b0b2602ab197a81a40997b785ad4
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-06 15:40:32 +0530

    Removing C9 DevEnv

--- main.tf
+++ main.tf
@@ -10,11 +10,3 @@ provider "aws" {
   access_key = var.key
   secret_key = var.secret_key
 }
-
-resource "aws_cloud9_environment_ec2" "DevEnv" {
-  instance_type = "t2.micro"
-  name          = "DevEnv"
-  image_id      = "amazonlinux-2023-x86_64"
-  automatic_stop_time_minutes = "30"
-  
-}
