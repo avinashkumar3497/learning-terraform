cb76804b0a41912699a98f10d1541d41dcaab7f4
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-07 02:33:54 +0530

    removing demo ec2_instance

--- main.tf
+++ main.tf
@@ -8,12 +8,3 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
 }
-
-resource "aws_instance" "demo_server" {
-  ami           = "ami-00beae93a2d981137"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld3"
-  }
-}
