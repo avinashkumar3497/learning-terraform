226516308c1fbac959cdc97f55f5285b6f333cfe
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-12 18:03:33 +0530

    Creating demo ec2 inst in New AWS account

--- main.tf
+++ main.tf
@@ -8,3 +8,12 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
 }
+
+resource "aws_instance" "demo_server" {
+  ami           = "ami-00beae93a2d981137"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
