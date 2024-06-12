b9128e939872d5a21fd937acefd93b7cf90cca6a
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-12 19:03:12 +0530

    setting budget alerts

--- main.tf
+++ main.tf
@@ -8,3 +8,30 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
 }
+
+resource "aws_budgets_budget" "one_dollar" {
+  time_unit    = "MONTHLY"
+  budget_type  = "COST"
+  limit_amount = "1"
+  limit_unit   = "USD"
+}
+
+resource "aws_budgets_budget" "five_dollar" {
+  time_unit    = "MONTHLY"
+  budget_type  = "COST"
+  limit_amount = "5"
+  limit_unit   = "USD"
+}
+
+resource "aws_budgets_budget" "ten_dollar" {
+  time_unit    = "MONTHLY"
+  budget_type  = "COST"
+  limit_amount = "10"
+  limit_unit   = "USD"
+}
+
+#resource "<provider>_<resource_type>" "name" {
+# config options...
+# key = "value" 
+# key2 = "another value"
+#}
