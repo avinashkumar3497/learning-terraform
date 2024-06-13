9f14841b103d65277dea6c51df7132b0643f7f50
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-13 16:34:44 +0530

    renamed cloud9 resource

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_cloud9_environment_ec2" "example" {
+resource "aws_cloud9_environment_ec2" "DevEnv" {
   instance_type = "t2.micro"
   name          = "DevEnv"
   image_id      = "amazonlinux-2023-x86_64"
