983704c38c9a1d509e038acaf3947cbdf48eb0ff
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-14 20:34:12 +0530

    Adding Elastic Beanstalk service role

--- iam.tf
+++ iam.tf
@@ -26,3 +26,33 @@ resource "aws_iam_role" "eb_ec2-role" {
     "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
     ]
 }
+
+resource "aws_iam_role" "aws-elasticbeanstalk-service-role" {
+  name                = "aws-elasticbeanstalk-service-role"
+
+  //it declares the trusted entity for the role.
+  //While creating a role in IAM. In the review section it shows this trust policy.
+  assume_role_policy  = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "elasticbeanstalk.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole",
+        "Condition": {
+          "StringEquals": {
+            "sts:ExternalId": "elasticbeanstalk"
+          }
+        }
+      }
+    ]
+})
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkEnhancedHealth",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy"
+    ]
+}
