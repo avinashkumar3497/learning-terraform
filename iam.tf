a1d83943d28573f123cd7b2f908c5224c6fb52b7
Avinash Kumar <avinashkumar3497@gmail.com>
2024-06-14 20:52:50 +0530

    destroying incorrect service role for EB

--- iam.tf
+++ iam.tf
@@ -27,32 +27,32 @@ resource "aws_iam_role" "eb_ec2-role" {
     ]
 }
 
-resource "aws_iam_role" "aws-elasticbeanstalk-service-role" {
-  name                = "aws-elasticbeanstalk-service-role"
+# resource "aws_iam_role" "aws-elasticbeanstalk-service-role" {
+#   name                = "aws-elasticbeanstalk-service-role"
 
-  //it declares the trusted entity for the role.
-  //While creating a role in IAM. In the review section it shows this trust policy.
-  assume_role_policy  = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "elasticbeanstalk.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole",
-        "Condition": {
-          "StringEquals": {
-            "sts:ExternalId": "elasticbeanstalk"
-          }
-        }
-      }
-    ]
-})
+#   //it declares the trusted entity for the role.
+#   //While creating a role in IAM. In the review section it shows this trust policy.
+#   assume_role_policy  = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Sid": "",
+#         "Effect": "Allow",
+#         "Principal": {
+#           "Service": "elasticbeanstalk.amazonaws.com"
+#         },
+#         "Action": "sts:AssumeRole",
+#         "Condition": {
+#           "StringEquals": {
+#             "sts:ExternalId": "elasticbeanstalk"
+#           }
+#         }
+#       }
+#     ]
+# })
 
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth",
-    "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy"
-    ]
-}
+#   managed_policy_arns = [
+#     "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth",
+#     "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy"
+#     ]
+# }
