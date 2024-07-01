4f3c2f782d9538d93c91b995cab8f563fe1adcbb
Avinash Kumar <avinashkumar3497@gmail.com>
2024-07-01 10:39:33 +0530

    created new ingress rule for new c9 env

--- security_groups.tf
+++ security_groups.tf
@@ -16,3 +16,22 @@ resource "aws_security_group_rule" "tcp_8080" {
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = data.aws_security_group.cloud9_secgroup.id
 }
+
+data "aws_security_group" "cloud9_secgroup2" {
+  filter {
+    name = "tag:aws:cloud9:environment"
+    values = [
+      aws_cloud9_environment_ec2.DevEnv2.id
+    ]
+  }
+}
+
+resource "aws_security_group_rule" "tcp_8080_2" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = data.aws_security_group.cloud9_secgroup2.id
+}
