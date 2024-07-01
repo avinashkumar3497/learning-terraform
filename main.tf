ae5e874a71b5d7cd67d432140daffd487a6f9103
Avinash Kumar <avinashkumar3497@gmail.com>
2024-07-01 10:38:57 +0530

    removing the test eb app & env

--- main.tf
+++ main.tf
@@ -9,31 +9,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_elastic_beanstalk_application" "tftest" {
-  name        = "tf-test-name"
-  description = "tf-test-desc"
-}
+# resource "aws_elastic_beanstalk_application" "tftest" {
+#   name        = "tf-test-name"
+#   description = "tf-test-desc"
+# }
 
-resource "aws_elastic_beanstalk_environment" "tfenvtest-demo" {
-  name                = "tf-test-name-demo"
-  application         = aws_elastic_beanstalk_application.tftest.name
-  solution_stack_name = "64bit Amazon Linux 2023 v6.1.6 running Node.js 20"
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = "aws-elasticbeanstalk-ec2-instance-profile"
-  }
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "SingleInstance"
-  }
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "EC2KeyName"
-    value     = "avinash-aws"
-  }
-}
+# resource "aws_elastic_beanstalk_environment" "tfenvtest-demo" {
+#   name                = "tf-test-name-demo"
+#   application         = aws_elastic_beanstalk_application.tftest.name
+#   solution_stack_name = "64bit Amazon Linux 2023 v6.1.6 running Node.js 20"
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "IamInstanceProfile"
+#     value     = "aws-elasticbeanstalk-ec2-instance-profile"
+#   }
+#   setting {
+#     namespace = "aws:elasticbeanstalk:environment"
+#     name      = "EnvironmentType"
+#     value     = "SingleInstance"
+#   }
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "EC2KeyName"
+#     value     = "avinash-aws"
+#   }
+# }
 
 #resource "<provider>_<resource_type>" "name" {
 # config options...
