9ed56b53c0dff56e71b45ccda37ac9bb5a2b3f7e
Avinash Kumar <avinashkumar3497@gmail.com>
2024-07-02 14:09:39 +0530

    removed old c9 env

--- cloud9.tf
+++ cloud9.tf
@@ -1,11 +1,11 @@
-resource "aws_cloud9_environment_ec2" "DevEnv" {
-  instance_type = "t2.micro"
-  name          = "DevEnv"
-  image_id      = "amazonlinux-2023-x86_64"
-    tags = {
-    Terraform = "true"
-  }
-}
+# resource "aws_cloud9_environment_ec2" "DevEnv" {
+#   instance_type = "t2.micro"
+#   name          = "DevEnv"
+#   image_id      = "amazonlinux-2023-x86_64"
+#     tags = {
+#     Terraform = "true"
+#   }
+# }
 
 resource "aws_cloud9_environment_ec2" "DevEnv2" {
   instance_type = "t2.micro"
